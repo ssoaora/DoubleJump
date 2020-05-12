f8f51ee4e5d04cdc113fd3210bb59ae4bbb2999b
aesop <63569586+aesoppppp@users.noreply.github.com>
2020-05-12 11:17:59 +0900

    Update to v0.0.3
    added /djump to toggle DoubleJump.
    (permission jump.DoubleJump)
    May collide with fly on survival plugin
    If it keeps colliding, rollback to v0.0.2

--- src/main/java/jump/doublejump/DoubleJump.java
+++ src/main/java/jump/doublejump/DoubleJump.java
@@ -11,32 +11,61 @@ import cn.nukkit.event.player.PlayerToggleFlightEvent;
 import cn.nukkit.level.Level;
 import cn.nukkit.level.Sound;
 import cn.nukkit.math.Vector3;
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DoubleJump extends PluginBase implements Listener {
 	
+	List<String> toggled = new ArrayList<String>();
+	
 	public void onEnable() {
 		this.getLogger().notice("§eThis Plugin Is Made By aesoppppp");
-        this.getLogger().info("§dDoubleJump v0.0.2 Is Enabled!");
+        this.getLogger().info("§dDoubleJump v0.0.3 Is Enabled!");
 		getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(label.equalsIgnoreCase("djump")) {
+			if(!(sender instanceof Player)) {
+				sender.sendMessage("§cYou must be a player to run this command!");
+				return false;
+			}
+			Player player = (Player) sender;
+			if(toggled.contains(player.getName())) {
+				player.sendMessage("§cYou are not Double Jumping!");
+				toggled.remove(player.getName());
+				return true;
+			}
+			player.sendMessage("§7[§6!§7] §fYou are now Double Jumping!");
+			toggled.add(player.getName());
+			return true;
+		}
+		return false;
 	}
 	
 	@EventHandler
 	public void onPlayerToggleFlight(PlayerToggleFlightEvent event) {
 		Player player = event.getPlayer();
-		if(player.isCreative()) 
+		if(player.isCreative() && !toggled.contains(player.getName()))
 			return;
 		event.setCancelled(true);
 		player.setAllowFlight(false);
+		if (toggled.contains(player.getName())) {
 		player.setMotion(player.getLocation().getDirectionVector().multiply(2.5D).add(0.0D, 1.5D, 0.0D));
 		Level level = player.getLevel();
 		level.addSound(player.getLocation(), Sound.MOB_ENDERDRAGON_FLAP);
+		}
 	}
 	
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent event) {
 		Player player = event.getPlayer();
 		if(!player.isCreative() 
+				&& toggled.contains(player.getName())
 				&& player.isOnGround() 
 				&& !player.getAllowFlight())
 			player.setAllowFlight(true);
